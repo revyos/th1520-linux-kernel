c-sky/csky-linux

driver: mmc: Add sophgo sg2042 soc support
Signed-off-by: Xiaoguang Xing <xiaoguang.xing@sophgo.com>

drivers/mmc/host/sdhci.c
@@ -49,7 +49,9 @@ static unsigned int debug_quirks2;
 
 static void sdhci_enable_preset_value(struct sdhci_host *host, bool enable);
 
+#ifndef CONFIG_SOC_SOPHGO
 static bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd);
+#endif
 
 void sdhci_dumpregs(struct sdhci_host *host)
 {
@@ -1627,7 +1629,11 @@ static void sdhci_finish_data(struct sdhci_host *host)
 	__sdhci_finish_data(host, false);
 }
 
+#ifndef CONFIG_SOC_SOPHGO
 static bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
+#else
+bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
+#endif
 {
 	int flags;
 	u32 mask;
@@ -1717,6 +1723,9 @@ static bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 
 	return true;
 }
+#ifdef CONFIG_SOC_SOPHGO
+EXPORT_SYMBOL_GPL(sdhci_send_command);
+#endif
 
 static bool sdhci_present_error(struct sdhci_host *host,
 				struct mmc_command *cmd, bool present)

drivers/mmc/host/sdhci.h
@@ -823,5 +823,8 @@ void sdhci_abort_tuning(struct sdhci_host *host, u32 opcode);
 void sdhci_switch_external_dma(struct sdhci_host *host, bool en);
 void sdhci_set_data_timeout_irq(struct sdhci_host *host, bool enable);
 void __sdhci_set_timeout(struct sdhci_host *host, struct mmc_command *cmd);
+#ifdef CONFIG_SOC_SOPHGO
+bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd);
+#endif
 
 #endif /* __SDHCI_HW_H */

revyos/th1520-linux-kernel

Signed-off-by: Mingzheng Xing <xingmingzheng@iscas.ac.cn>

drivers/mmc/host/sdhci.c
@@ -49,7 +49,9 @@ static unsigned int debug_quirks2;
 
 static void sdhci_enable_preset_value(struct sdhci_host *host, bool enable);
 
+#ifndef CONFIG_ARCH_SOPHGO
 static bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd);
+#endif
 
 void sdhci_dumpregs(struct sdhci_host *host)
 {
@@ -1627,7 +1629,11 @@ static void sdhci_finish_data(struct sdhci_host *host)
 	__sdhci_finish_data(host, false);
 }
 
+#ifndef CONFIG_ARCH_SOPHGO
 static bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
+#else
+bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
+#endif
 {
 	int flags;
 	u32 mask;
@@ -1717,6 +1723,9 @@ static bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 
 	return true;
 }
+#ifdef CONFIG_ARCH_SOPHGO
+EXPORT_SYMBOL_GPL(sdhci_send_command);
+#endif
 
 static bool sdhci_present_error(struct sdhci_host *host,
 				struct mmc_command *cmd, bool present)

drivers/mmc/host/sdhci.h
@@ -823,5 +823,8 @@ void sdhci_abort_tuning(struct sdhci_host *host, u32 opcode);
 void sdhci_switch_external_dma(struct sdhci_host *host, bool en);
 void sdhci_set_data_timeout_irq(struct sdhci_host *host, bool enable);
 void __sdhci_set_timeout(struct sdhci_host *host, struct mmc_command *cmd);
+#ifdef CONFIG_ARCH_SOPHGO
+bool sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd);
+#endif
 
 #endif /* __SDHCI_HW_H */