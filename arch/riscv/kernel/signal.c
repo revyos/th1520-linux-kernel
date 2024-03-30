4be52eb9f0515af49f24c4f70fe91cafaba36ef1
Han Gao <gaohan@iscas.ac.cn>
2024-03-30 22:32:45 +0800

    Revert "riscv: sched: defer restoring Vector context for user"
    This reverts commit 7df56cbc27e4239807b5d8860f79a7350d63a741.

--- arch/riscv/kernel/signal.c
+++ arch/riscv/kernel/signal.c
@@ -86,10 +86,7 @@ static long save_v_state(struct pt_regs *regs, void __user **sc_vec)
 	/* datap is designed to be 16 byte aligned for better performance */
 	WARN_ON(unlikely(!IS_ALIGNED((unsigned long)datap, 16)));
 
-	get_cpu_vector_context();
 	riscv_v_vstate_save(current, regs);
-	put_cpu_vector_context();
-
 	/* Copy everything of vstate but datap. */
 	err = __copy_to_user(&state->v_state, &current->thread.vstate,
 			     offsetof(struct __riscv_v_ext_state, datap));
@@ -137,7 +134,7 @@ static long __restore_v_state(struct pt_regs *regs, void __user *sc_vec)
 	if (unlikely(err))
 		return err;
 
-	riscv_v_vstate_set_restore(current, regs);
+	riscv_v_vstate_restore(current, regs);
 
 	return err;
 }
