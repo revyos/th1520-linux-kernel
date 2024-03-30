99a74ec75a88e57c659dad3ffb5018cc6e7e8568
Han Gao <gaohan@iscas.ac.cn>
2024-03-30 22:32:51 +0800

    Revert "riscv: Add support for kernel mode vector"
    This reverts commit ecd2ada8a5e0b464dab54f71d4ba7bbf5708711f.

--- arch/riscv/include/asm/processor.h
+++ arch/riscv/include/asm/processor.h
@@ -72,15 +72,6 @@
 struct task_struct;
 struct pt_regs;
 
-/*
- * We use a flag to track in-kernel Vector context. Currently the flag has the
- * following meaning:
- *
- *  - bit 0: indicates whether the in-kernel Vector context is active. The
- *    activation of this state disables the preemption.
- */
-#define RISCV_KERNEL_MODE_V	0x1
-
 /* CPU-specific state of a task */
 struct thread_struct {
 	/* Callee-saved registers */
@@ -89,8 +80,7 @@ struct thread_struct {
 	unsigned long s[12];	/* s[0]: frame pointer */
 	struct __riscv_d_ext_state fstate;
 	unsigned long bad_cause;
-	u32 riscv_v_flags;
-	u32 vstate_ctrl;
+	unsigned long vstate_ctrl;
 	struct __riscv_v_ext_state vstate;
 	unsigned long align_ctl;
 };
