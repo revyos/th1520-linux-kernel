61c05e2a1879bd08a5c31873f03e60c057664199
Vivian Wang <wangruikang@iscas.ac.cn>
2026-01-29 09:56:09 +0800

    UPSTREAM: ALSA: hda/intel: Make MSI address limit based on the device DMA limit
    The hda/intel driver restricts the MSI message address for devices which do
    not advertise full 64-bit DMA address space support to 32-bit due to the
    former restrictions of the PCI/MSI code which only allowed either 32-bit or
    a full 64-bit address range.
    
    This does not work on platforms which have a MSI doorbell address above the
    32-bit boundary but do not support the full 64 bit address range.
    
    The PCI/MSI core converted this binary decision to a DMA_BIT_MASK() based
    decision, which allows to describe the device limitations precisely.
    
    Convert the driver to provide the exact DMA address limitations to the
    PCI/MSI core. That allows devices which do not support the full 64-bit
    address space to work on platforms which have a MSI doorbell address above
    the 32-bit limit as long as it is within the hardware's addressable range.
    
    [ tglx: Massage changelog ]
    
    Signed-off-by: Vivian Wang <wangruikang@iscas.ac.cn>
    Signed-off-by: Thomas Gleixner <tglx@kernel.org>
    Acked-by: Takashi Iwai <tiwai@suse.de>
    Link: https://patch.msgid.link/20260129-pci-msi-addr-mask-v4-4-70da998f2750@iscas.ac.cn
    (cherry picked from commit cb9b6f9d2be6bda1b0117b147df40f982ce06888)
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -1915,15 +1915,6 @@ static int azx_first_init(struct azx *chip)
 		chip->gts_present = true;
 #endif
 
-	if (chip->msi) {
-		if (chip->driver_caps & AZX_DCAPS_NO_MSI64) {
-			dev_dbg(card->dev, "Disabling 64bit MSI\n");
-			pci->msi_addr_mask = DMA_BIT_MASK(32);
-		}
-		if (pci_enable_msi(pci) < 0)
-			chip->msi = 0;
-	}
-
 	pci_set_master(pci);
 
 	gcap = azx_readw(chip, GCAP);
@@ -1973,6 +1964,15 @@ static int azx_first_init(struct azx *chip)
 	if (dma_set_mask_and_coherent(&pci->dev, DMA_BIT_MASK(dma_bits)))
 		dma_set_mask_and_coherent(&pci->dev, DMA_BIT_MASK(32));
 	dma_set_max_seg_size(&pci->dev, UINT_MAX);
+
+	if (chip->msi) {
+		if (chip->driver_caps & AZX_DCAPS_NO_MSI64) {
+			dev_dbg(card->dev, "Restricting MSI to %u-bit\n", dma_bits);
+			pci->msi_addr_mask = DMA_BIT_MASK(dma_bits);
+		}
+		if (pci_enable_msi(pci) < 0)
+			chip->msi = 0;
+	}
 
 	/* read number of streams from GCAP register instead of using
 	 * hardcoded value
